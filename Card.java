b35a67fb1c1df6e6e902564fab86ba548568a0b0
Fiszic <112727547+Fiszic@users.noreply.github.com>
2022-09-08 07:59:49 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,4 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.util.*;
 
-public class Card {
-
+public class Card  extends Deck{
+	private BufferedImage CardImage;
+	private String CardName;
+	private int number;
+	public Card(String cn, int n, BufferedImage CI) {
+		CardName = cn;
+		number = n;
+		CardImage = CI;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public BufferedImage getImage() {
+		return CardImage;
+	}
 }
