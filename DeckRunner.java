adc615f894928452b60ca258e2f07a48b4a9131f
Fiszic <112727547+Fiszic@users.noreply.github.com>
2022-09-08 08:00:41 -0500

    Update DeckRunner.java

--- DeckRunner.java
+++ DeckRunner.java
@@ -1,4 +1,14 @@
-
-public class DeckRunner {
+import java.awt.*;
+import javax.swing.*;
+public class DeckRunner extends JFrame{
+	private static final int WIDTH = 1500;
+	private static final int HEIGHT = 900;
+	public DeckRunner(String title) {
+		super(title);
+		setSize(WIDTH, HEIGHT);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		add(new Deck());
+		setVisible(true);
+	}
 
 }
