6a4e84960230485da5d8b4dba27db6e54a9dbdfe
Fiszic <112727547+Fiszic@users.noreply.github.com>
2022-09-18 22:54:51 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,18 +1,19 @@
-
+import java.util.*;
 
 public class Player {
 	private String name;
 	private int coordx, coordy;
-	private Card[] hand;
+	private ArrayList<Card> hand;
 	private int tokens;
-	private boolean inGame;
+	private boolean inGame, immunity;
+	private ArrayList<Card> discard;
 	public Player(String n/*, Card c, int x, int y*/) {
-		hand = new Card[2];
-		hand[0] = null;
-		hand[1] = null;
+		hand = new ArrayList<>();
 		name = n;
 		inGame = true;
 		tokens = 0;
+		discard = new ArrayList<>();
+		immunity = false;
 		//coordx = x;
 		//coordy=y;
 		
@@ -27,23 +28,68 @@ public class Player {
 		return name;
 	}
 	public void drawCard(Card c) {
-		if(hand[0] == null) {
-			hand[0] = c;
-		}
-		else if(hand[1] == null) {
-			hand[1] = c;
-		}
-		else {
-			System.out.println("Hand is full");
-		}
+		hand.add(c);
+		//System.out.println(hand.get(0).getName() + hand.get(1).getName());
 	}
 	public void addToken() {
 		tokens++;
 	}
-	public void useCard(int i) {
-		hand[i] = null;
+	public void discardCard(int i) {
+		Card c = hand.remove(i);
+		if(c.getCardNumber() == 9) {
+			lose();
+		}
+		discard.add(c);
 	}
 	public boolean isInGame() {
 		return inGame;
+	}
+	public int getTokens() {
+		return tokens;
+	}
+	public void lose() {
+		inGame = false;
+	}
+	public int getHandAmmount() {
+		return hand.size();
+	}
+	public Card getHandCard(int i) {
+		return hand.get(i);
+	}
+	public ArrayList<Card> getDiscard(){
+		return discard;
+	}
+	public void addDiscardCardDebug(Card c) {
+		discard.add(c);
+	}
+	public void immunityTrue() {
+		immunity = true;
+	}
+	public void immunityFalse() {
+		immunity = false;
+	}
+	public boolean immune() {
+		return immunity;
+	}
+	public void swapHand(Card c) {
+		hand.add(c);
+		hand.remove(0);
+	}
+	public int getSpyUsedCount() {
+		int count = 0;
+		for(int i = 0; i<discard.size();i++) {
+			if(discard.get(i).getCardNumber() == 0) {
+				count ++;
+			}
+		}
+		return count;
+	}
+	public void reset() {
+		hand.clear();
+		discard.clear();
+		inGame = true;
+	}
+	public void removeCard(int i) {
+		hand.remove(i);
 	}
 }
